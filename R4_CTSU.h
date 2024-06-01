bad328b80586b2eb80d8c639964f601cf54e27c4
david <6957239+delta-G@users.noreply.github.com>
2024-05-31 22:02:03 -0500

    empty

---
+++ R4_CTSU.h
@@ -0,0 +1,31 @@
+/*
+
+R4_CTSU.h  --  Capacitive Touch Sensing for Arduino UNO-R4
+     Copyright (C) 2023  David C.
+
+     Inlcudes code that was contributed to the LoveButton library 
+     by Winnie S.
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+     */
+
+#ifndef R4_CTSU_H
+#define R4_CTSU_H
+
+
+
+
+
+#endif //R4_CTSU_H
---
+++ R4_CTSU_Test.ino
@@ -0,0 +1,13 @@
+void setup() {
+
+  Serial.begin(115200);
+  while(!Serial);
+  Serial.println("\n\n *** " R4_CTSU_Test.ino " ***\n\n");
+
+  
+
+}
+
+void loop() {
+
+}
